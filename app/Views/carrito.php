e3b785e3600b1e3830e8931561fd099367415c45
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-06-16 12:48:44 -0300

    se volvio responsive la tabla

--- app/Views/carrito.php
+++ app/Views/carrito.php
@@ -7,39 +7,41 @@
             <?php if ($cart->contents() == NULL) { ?>
                 <h2 class="text-center alert-danger">Carrito está vacío</h2>
             <?php } else { ?>
-                <table id="mytable" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>N° item</th>
-                            <th>Nombre</th>
-                            <th>Precio</th>
-                            <th>Cantidad</th>
-                            <th>Subtotal</th>
-                            <th>Acción</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $total = 0;
-                        $i = 1;
-                        foreach ($cart->contents() as $item) : ?>
+                <div class="table-responsive">
+                    <table id="mytable" class="table table-bordered table-striped">
+                        <thead>
                             <tr>
-                                <td><?= $i++ ?></td>
-                                <td><?= esc($item['name']) ?></td>
-                                <td><?= esc($item['price']) ?></td>
-                                <td><?= esc($item['qty']) ?></td>
-                                <td><?= esc($item['subtotal']);
-                                    $total += $item['subtotal'] ?></td>
-                                <td><?= anchor('/remove_item/' . $item['rowid'], 'Eliminar',['class' => 'btn btn-danger text-white']); ?></td>
+                                <th>N° item</th>
+                                <th>Nombre</th>
+                                <th>Precio</th>
+                                <th>Cantidad</th>
+                                <th>Subtotal</th>
+                                <th>Acción</th>
                             </tr>
-                        <?php endforeach; ?>
-                        <tr>
-                            <td colspan="4">Total Compra: $<?= esc($total) ?></td>
-                            <td><?= anchor('/delete_cart','Vaciar Carrito', ['class' => 'btn btn-danger text-white']) ;?></td>
-                            <td><a href="ventas" class="btn btn-success text-white" role="button">Ordenar Compra</a></td>
-                        </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $total = 0;
+                            $i = 1;
+                            foreach ($cart->contents() as $item) : ?>
+                                <tr>
+                                    <td><?= $i++ ?></td>
+                                    <td><?= esc($item['name']) ?></td>
+                                    <td><?= esc($item['price']) ?></td>
+                                    <td><?= esc($item['qty']) ?></td>
+                                    <td><?= esc($item['subtotal']);
+                                        $total += $item['subtotal'] ?></td>
+                                    <td><?= anchor('/remove_item/' . $item['rowid'], 'Eliminar', ['class' => 'btn btn-danger text-white']); ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            <tr>
+                                <td colspan="4">Total Compra: $<?= esc($total) ?></td>
+                                <td><?= anchor('/delete_cart', 'Vaciar Carrito', ['class' => 'btn btn-danger text-white']); ?></td>
+                                <td><a href="ventas" class="btn btn-success text-white" role="button">Ordenar Compra</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             <?php } ?>
         </div>
     </div>
