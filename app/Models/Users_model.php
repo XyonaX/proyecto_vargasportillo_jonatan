9b50b6285f35e8b0a6560ebed636ad885629f0e6
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-06-17 17:31:58 -0300

    se agrego el campo usuario_estado

--- app/Models/Users_model.php
+++ app/Models/Users_model.php
@@ -10,7 +10,7 @@ class Users_model extends Model
     protected $primaryKey = 'usuario_id';
     protected $useAutoIncrement = true;
     protected $allowedFields = [
-        'rol_id', 'usuario_nombre', 'usuario_apellido','usuario_direccion','usuario_email','usuario_password'
+        'rol_id', 'usuario_nombre', 'usuario_apellido','usuario_direccion','usuario_email','usuario_password','usuario_estado'
     ];
     protected $returnType = 'array';
     
