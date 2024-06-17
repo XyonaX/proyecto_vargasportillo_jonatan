9f9860cea778c45d9668c15bdc0eada80fdcb236
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-06-17 02:41:40 -0300

    se modifico la ruta

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -46,7 +46,7 @@ $routes->get('/remove_item/(:any)', 'Carrito::eliminar_item/$1');
 $routes->get('/delete_cart', 'Carrito::eliminar_carrito');
 $routes->get('/ventas', 'Carrito::guardar_venta');
 $routes->get('/ver_ventas', 'Ventas::index',['filter' => 'admin']);
-$routes->get('/ver_detalle/(:num)', 'Ventas::listar_detalle_venta/$1',['filter' => 'admin']);
+$routes->get('/ventas/factura/(:num)', 'Ventas::factura/$1', ['filter' => 'admin']);
 
 $routes->post('/contact', 'Contact::add_consulta');
 $routes->post('/register', 'Users::register_user');
