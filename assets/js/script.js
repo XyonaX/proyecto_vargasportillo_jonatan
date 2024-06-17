850417163d8e186d3ac8aa1971fc3dfcced3b9b3
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-06-16 23:34:30 -0300

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,25 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('content').style.display = 'block';
     }, 2000); // Cambia 3000 por el tiempo en milisegundos que desees
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const editButtons = document.querySelectorAll('.edit');
+
+    editButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const productId = button.getAttribute('data-id');
+            const nombre = button.getAttribute('data-nombre');
+            const descripcion = button.getAttribute('data-descripcion');
+            const precio = button.getAttribute('data-precio');
+            const cantidad = button.getAttribute('data-cantidad');
+            const activo = button.getAttribute('data-activo');
+
+            document.getElementById('editProductId').value = productId;
+            document.getElementById('edit_nombre').value = nombre;
+            document.getElementById('edit_descripcion').value = descripcion;
+            document.getElementById('edit_precio').value = precio;
+            document.getElementById('edit_cantidad').value = cantidad;
+            document.getElementById('edit_activo').value = activo;
+        });
+    });
+});
