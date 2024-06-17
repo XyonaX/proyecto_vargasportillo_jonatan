9d6e30b8490c0a479ecf43f4f24062b98d709580
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-06-17 03:19:45 -0300

    se agrego mpdf

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -42,7 +42,8 @@ class Autoload extends AutoloadConfig
     public $psr4 = [
         APP_NAMESPACE => APPPATH, // For custom app namespace
         'Config'      => APPPATH . 'Config',
-        'CodeIgniterCart' => ROOTPATH . 'Cart/src'
+        'CodeIgniterCart' => ROOTPATH . 'Cart/src',
+        'CodeIgniterCart' => ROOTPATH . 'mpdf/src'
     ];
 
     /**
