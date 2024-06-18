ac95fcab2ccaef6d95868f110bdde78d62d48c65
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-06-17 21:47:24 -0300

    se agrego mas links aunque no este activos

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,42 +1,81 @@
-    </main>
-    <footer class="container-fluid bg-dark mt-auto w">
+</main>
+<footer class="container-fluid bg-dark text-white mt-auto">
+    <div class="container py-4">
         <div class="row">
-            <div class="col">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                    <div class="d-md-flex gap-3">
-                        <li class="nav-item mt-2" style="list-style: none;">
-                            <a class="nav-link" href="<?php echo base_url('/commercialization'); ?>">Comercialización</a>
-                        </li>
-                        <li class="nav-item mt-2" style="list-style: none;">
-                            <a class="nav-link" href="<?php echo base_url('/terms'); ?>">Terms&Usos</a>
-                        </li>
-                    </div>
-                    <div class="social gap-2 col w-100 d-flex flex-row justify-content-center mt-2">
-                        <p class="text-white d-none d-sm-block">______________________________</p>
-                        <a class="text-white" href="https://google.com" target="_blank"><i class="bi bi-instagram"></i></a>
-                        <a class="text-white" href="https://google.com" target="_blank"><i class="bi bi-linkedin"></i></a>
-                        <a class="text-white" href="https://google.com" target="_blank"><i class="bi bi-twitter"></i></a>
-                        <p class="text-white d-none d-sm-block">______________________________</p>
-                    </div>
-                    <div class="col d-flex flex-column flex-sm-row justify-content-center align-items-center">
-                        <p class="text-white mb-0">Copyrights©2024</p>
-                        <p class="text-white ms-2 mb-0 mb-sm-0">Development by <a class="text-white text-decoration-none" href="">Vargas Jonatan</a></p>
-                    </div>
+            <div class="col-md-3">
+                <h5>Sobre Nosotros</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/about'); ?>">Nuestra Historia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/terms'); ?>">Terms & Cond</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/commercialization'); ?>">Comercializacion</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Contacto</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/contact'); ?>">Contáctanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/support'); ?>">Soporte</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/faq'); ?>">Preguntas Frecuentes</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Servicios</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/services'); ?>">Nuestros Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/pricing'); ?>">Precios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="<?php echo base_url('/testimonials'); ?>">Testimonios</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Redes Sociales</h5>
+                <div class="d-flex justify-content-start gap-2">
+                    <a class="text-white" href="https://instagram.com" target="_blank"><i class="bi bi-instagram"></i></a>
+                    <a class="text-white" href="https://linkedin.com" target="_blank"><i class="bi bi-linkedin"></i></a>
+                    <a class="text-white" href="https://twitter.com" target="_blank"><i class="bi bi-twitter"></i></a>
+                    <a class="text-white" href="https://facebook.com" target="_blank"><i class="bi bi-facebook"></i></a>
                 </div>
             </div>
         </div>
-    </footer>
-    </section>
+        <hr class="bg-light">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="mb-0">&copy; 2024 Todos los derechos reservados.</p>
+            </div>
+            <div class="col-md-6 text-md-end">
+                <p class="mb-0">Desarrollado por <a class="text-white text-decoration-none" href="#">Vargas Jonatan</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+</section>
 
 
 
-    <script src="<?= base_url('assets/js/script.js') ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.11.1/dist/sweetalert2.all.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/js/script.js') ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.11.1/dist/sweetalert2.all.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-    </body>
+</body>
 
-    </html>
+</html>
